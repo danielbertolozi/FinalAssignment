6d4b0fad98c34794a7de017ddce5e77b2f2861bb
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-24 12:49:58 -0300

    Adds mappings for models

--- FinalAssignment/Models/Mappings/MedicsMap.cs
+++ FinalAssignment/Models/Mappings/MedicsMap.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace FinalAssignment.Models.Mappings
 {
 	public class MedicsMap
 	{
-		public MedicsMap()
+		public MedicsMap(EntityTypeBuilder<Medics> entityBuilder)
 		{
+			entityBuilder.HasKey(t => t.MedicKey);
+
+			entityBuilder.Property(t => t.MedicKey)
+			             .ValueGeneratedOnAdd();
+
+			entityBuilder.Property(t => t.Address);
+			entityBuilder.Property(t => t.BirthDate);
+			entityBuilder.Property(t => t.Deleted);
+			entityBuilder.Property(t => t.Email);
+			entityBuilder.Property(t => t.Genre);
+			entityBuilder.Property(t => t.Name);
+			entityBuilder.Property(t => t.Password);
+			entityBuilder.Property(t => t.Phone);
+
+			entityBuilder.ToTable("Medics");
+			entityBuilder.Property(t => t.Address).HasColumnName("Address");
+			entityBuilder.Property(t => t.BirthDate).HasColumnName("BirthDate");
+			entityBuilder.Property(t => t.Deleted).HasColumnName("Deleted");
+			entityBuilder.Property(t => t.Email).HasColumnName("Email");
+			entityBuilder.Property(t => t.Genre).HasColumnName("Genre");
+			entityBuilder.Property(t => t.MedicKey).HasColumnName("MedicKey");
+			entityBuilder.Property(t => t.Name).HasColumnName("Name");
+			entityBuilder.Property(t => t.Password).HasColumnName("Password");
+			entityBuilder.Property(t => t.Phone).HasColumnName("Phone");
 		}
 	}
 }
--- FinalAssignment/Models/Mappings/PatientsMap.cs
+++ FinalAssignment/Models/Mappings/PatientsMap.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace FinalAssignment.Models.Mappings
 {
 	public class PatientsMap
 	{
-		public PatientsMap()
+		public PatientsMap(EntityTypeBuilder<Patients> entityBuilder)
 		{
+			entityBuilder.HasKey(t => t.PatientKey);
+
+			entityBuilder.Property(t => t.PatientKey)
+						 .ValueGeneratedOnAdd();
+
+			entityBuilder.Property(t => t.Address);
+			entityBuilder.Property(t => t.Consults);
+			entityBuilder.Property(t => t.BirthDate);
+			entityBuilder.Property(t => t.Deleted);
+			entityBuilder.Property(t => t.Email);
+			entityBuilder.Property(t => t.Genre);
+			entityBuilder.Property(t => t.Name);
+			entityBuilder.Property(t => t.Password);
+			entityBuilder.Property(t => t.Phone);
+
+			entityBuilder.ToTable("Medics");
+			entityBuilder.Property(t => t.Address).HasColumnName("Address");
+			entityBuilder.Property(t => t.BirthDate).HasColumnName("BirthDate");
+			entityBuilder.Property(t => t.Deleted).HasColumnName("Deleted");
+			entityBuilder.Property(t => t.Email).HasColumnName("Email");
+			entityBuilder.Property(t => t.Genre).HasColumnName("Genre");
+			entityBuilder.Property(t => t.PatientKey).HasColumnName("PatientKey");
+			entityBuilder.Property(t => t.Name).HasColumnName("Name");
+			entityBuilder.Property(t => t.Password).HasColumnName("Password");
+			entityBuilder.Property(t => t.Phone).HasColumnName("Phone");
 		}
 	}
 }
