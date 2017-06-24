79c1f5f157a72953433c957b422b1a6f4e238e40
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-24 11:57:14 -0300

    Adds models with DataAnnotations

--- FinalAssignment/Models/Availability.cs
+++ FinalAssignment/Models/Availability.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace FinalAssignment.Models
 {
 	public class Availability
 	{
 		public Availability()
 		{
+			this.Medics = new List<Medics>();
 		}
+		[Required]
+		[Range(1,7)]
+		public int DayOfWeek { get; set; }
+		[Required]
+		[RegularExpression("\\d{2}:\\d{2}", ErrorMessage = "Please provide time in the following fashion: [HH:mm]")]
+		public string StartTime { get; set; }
+		[Required]
+		[RegularExpression("\\d{2}:\\d{2}", ErrorMessage = "Please provide time in the following fashion: [HH:mm]")]
+		public string EndTime { get; set; }
+		[Required]
+		[Range(0,1)]
+		public int IsAllDay { get; set; }
+		public ICollection<Medics> Medics { get; set; }
 	}
 }
--- FinalAssignment/Models/Medics.cs
+++ FinalAssignment/Models/Medics.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace FinalAssignment.Models
 {
 	public class Medics
@@ -6,5 +9,33 @@ namespace FinalAssignment.Models
 		public Medics()
 		{
 		}
+		[Required]
+		[StringLength(64)]
+		public string Name { get; set; }
+		[Required]
+		[Timestamp]
+		[DisplayName("Date of Birth")]
+		public DateTime BirthDate { get; set; }
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		[StringLength(64)]
+		public string Email { get; set; }
+		[Required]
+		[DataType(DataType.PhoneNumber)]
+		[StringLength(14)]
+		public string Phone { get; set; }
+		[Required]
+		[StringLength(32)]
+		public string Address { get; set; }
+		[Required]
+		[RegularExpression("[mfMF]", ErrorMessage = "Please insert a valid genre value (M-F).")]
+		public char Genre { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[StringLength(32, MinimumLength = 8, ErrorMessage = "Please insert a password that is greater than 8 characters.")]
+		public string Password { get; set; }
+		[Required]
+		[Range(0,1)]
+		public int Deleted { get; set; }
 	}
 }
