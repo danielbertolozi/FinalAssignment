4b155d6ee880f58e3a515069fbb3b194752c6b21
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-24 13:02:43 -0300

    Removes misplaced character

--- FinalAssignment/Startup.cs
+++ FinalAssignment/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;\
+using Microsoft.Extensions.Logging;
 using FinalAssignment.Models;
 
 namespace FinalAssignment
