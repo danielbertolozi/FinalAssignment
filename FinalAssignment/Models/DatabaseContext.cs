4e6ac77d7bf84a481a482fdbd6b5b36458c431a8
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-24 12:57:18 -0300

    Instantiates Mappings for the Models

--- FinalAssignment/Models/DatabaseContext.cs
+++ FinalAssignment/Models/DatabaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using FinalAssignment.Models.Mappings;
 
 namespace FinalAssignment.Models
 {
@@ -10,9 +11,18 @@ namespace FinalAssignment.Models
 			optionsBuilder.UseSqlite("Filename=Database.db");
 		}
 
+		public DbSet<Availability> Availability { get; set; }
+		public DbSet<Consults> Consults { get; set; }
+		public DbSet<Medics> Medics { get; set; }
+		public DbSet<Patients> Patients { get; set; }
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
+			new AvailabilityMap(modelBuilder.Entity<Availability>());
+			new ConsultsMap(modelBuilder.Entity<Consults>());
+			new MedicsMap(modelBuilder.Entity<Medics>());
+			new PatientsMap(modelBuilder.Entity<Patients>());
 		}
 
 	}
