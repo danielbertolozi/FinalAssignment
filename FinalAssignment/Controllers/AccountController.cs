f546d623f1bd5e28f588a5de0f4c4fc09a8a2b08
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-28 00:58:54 -0300

    Creates basic controller for Account

--- FinalAssignment/Controllers/AccountController.cs
+++ FinalAssignment/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using FinalAssignment.ViewModels;
+
 namespace FinalAssignment.Controllers
 {
-	public class AccountController
+	public class AccountController : Controller
 	{
-		public AccountController()
+		[HttpGet]
+		public IActionResult Create()
 		{
+			return View();
+		}
+
+		[HttpPost]
+		public List<IActionResult> Create(CreateViewModel Model)
+		{
+			// TODO for this, create a class that will handle requests, and import it here
 		}
 	}
 }
