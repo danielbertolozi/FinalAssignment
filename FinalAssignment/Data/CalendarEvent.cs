a391f40733917a56edb4615ed1d080a9f8e7caca
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-29 22:27:35 -0300

    * CalendarEvent.cs: Adds missing fields
    * Agenda.cshtml: Prototype for List-based Agenda

--- FinalAssignment/Data/CalendarEvent.cs
+++ FinalAssignment/Data/CalendarEvent.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using FinalAssignment.Models;
 namespace FinalAssignment.Data
 {
 	public class CalendarEvent
 	{
-		public CalendarEvent(string title, string description, DateTime startTime, DateTime endTime)
+		public CalendarEvent(string title, string description, DateTime startTime, DateTime endTime, Medics medic, Patients patient)
 		{
 			this.Title = title;
 			this.Description = description;
 			this.StartTime = startTime;
 			this.EndTime = endTime;
+			this.Medic = medic;
+			this.Patient = patient;
 		}
 		public string Title { get; set; }
 		public string Description { get; set; }
+		public Medics Medic { get; set; }
+		public Patients Patient { get; set; }
 		public DateTime StartTime { get; set; }
 		public DateTime EndTime { get; set; }
+
+		/* TODO maybe change StartTime and EndTime for Date and Duration */
 	}
 }
