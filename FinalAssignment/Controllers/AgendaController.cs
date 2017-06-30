681d262e09fd3a797e3158531810414273b3b1d4
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-30 20:48:48 -0300

    Adds methods for loading the View's dropdowns

--- FinalAssignment/Controllers/AgendaController.cs
+++ FinalAssignment/Controllers/AgendaController.cs
@@ -7,6 +7,7 @@ using FinalAssignment.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FinalAssignment.Controllers
 {
@@ -30,8 +31,10 @@ namespace FinalAssignment.Controllers
 		[HttpGet]
 		public IActionResult Create()
 		{
+			string UserRole = User.Claims.Where(c => c.Type == "Role").FirstOrDefault().Value;
+			@ViewBag.AtendeesList = UserRole == "Patient" ? _GetMedicsList() : _GetPatientsList();
+			@ViewBag.ClassificationList = _GetClassifications();
 			return View();
-			/* TODO methods to fetch dropdown infos */
 		}
 
 		private List<Consults> _FetchEventsFromUser()
@@ -56,7 +59,43 @@ namespace FinalAssignment.Controllers
 				}
 			}
 			return ConsultsList;
+		}
 
+		private List<SelectListItem> _GetMedicsList()
+		{
+			using (DatabaseContext Database = new DatabaseContext())
+			{
+				List<Medics> Medics = Database.Medics.ToList();
+				List<SelectListItem> MedicsList = new List<SelectListItem>();
+				foreach (Medics Medic in Medics)
+				{
+					MedicsList.Add(new SelectListItem { Text = Medic.Name, Value = Medic.MedicKey.ToString() });
+				}
+				return MedicsList;
+			}
+		}
+
+		private List<SelectListItem> _GetPatientsList()
+		{
+			using (DatabaseContext Database = new DatabaseContext())
+			{
+				List<Patients> Patients = Database.Patients.ToList();
+				List<SelectListItem> PatientsList = new List<SelectListItem>();
+				foreach (Patients Patient in Patients)
+				{
+					PatientsList.Add(new SelectListItem { Text = Patient.Name, Value = Patient.PatientKey.ToString() });
+				}
+				return PatientsList;
+			}
+		}
+
+		private List<SelectListItem> _GetClassifications()
+		{
+			List<SelectListItem> ClassificationsList = new List<SelectListItem>();
+			ClassificationsList.Add(new SelectListItem { Text = "First Consult", Value = "1" });
+			ClassificationsList.Add(new SelectListItem { Text = "Re-consult", Value = "2" });
+			ClassificationsList.Add(new SelectListItem { Text = "Routine Consult", Value = "3" });
+			return ClassificationsList;
 		}
 	}
 }
