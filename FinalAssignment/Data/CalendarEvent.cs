3f5b8c878251c938f0a147eea17b82bb486958a1
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-29 22:13:19 -0300

    * CalendarEvent.cs: Adds Calendar Class
    * AgendaViewModel.cs: Add AgendaViewModel

---
+++ FinalAssignment/Data/CalendarEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FinalAssignment.Data
+{
+	public class CalendarEvent
+	{
+		public CalendarEvent(string title, string description, DateTime startTime, DateTime endTime)
+		{
+			this.Title = title;
+			this.Description = description;
+			this.StartTime = startTime;
+			this.EndTime = endTime;
+		}
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+	}
+}
