5efdb346f3f161fcc10008088ec6bf0dd8f606e8
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-30 01:01:16 -0300

    * FinalAssignment.csproj: Adds FluentValidator to Project   Dependencies + Folder for new Utilitaries
    * Medics.cs: Link the Unique Email Validator into Medics
    
    * Patients.cs: Link the Unique Email Validator into Patients
    
    * UniqueValidator.cs: Adds class to Validate Unique Constraints over
      Emails

---
+++ FinalAssignment/Util/UniqueValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using FinalAssignment.Models;
+using FluentValidation;
+
+namespace FinalAssignment.Util
+{
+	public class MedicsUniqueValidator : AbstractValidator<Medics>
+	{
+		public MedicsUniqueValidator()
+		{
+			RuleFor(x => x.Email).Must(BeUniqueEmail).WithMessage("Email already exists!");
+		}
+		private bool BeUniqueEmail (string Email)
+		{
+			return new DatabaseContext().Medics.FirstOrDefault(t => t.Email == Email) == null;
+		}
+	}
+	public class PatientsUniqueValidator : AbstractValidator<Patients>
+	{
+		public PatientsUniqueValidator()
+		{
+			RuleFor(x => x.Email).Must(BeUniqueEmail).WithMessage("Email already exists!");
+		}
+		private bool BeUniqueEmail(string Email)
+		{
+			return new DatabaseContext().Patients.FirstOrDefault(t => t.Email == Email) == null;
+		}
+	}
+}
