e738e2cde37486198590224e95d3f0e219cc444f
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-06-30 01:25:23 -0300

    * AgendaController.cs: Adds missing information
    * Agenda.cshtml: Corrects wrong expressions

--- FinalAssignment/Controllers/AgendaController.cs
+++ FinalAssignment/Controllers/AgendaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using FinalAssignment.Data;
 using FinalAssignment.ViewModels;
+using FinalAssignment.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalAssignment.Controllers
@@ -17,7 +18,7 @@ namespace FinalAssignment.Controllers
 			AgendaViewModel Model = new AgendaViewModel();
 			List<CalendarEvent> CalendarEventList = new List<CalendarEvent>();
 			EventGenerator Generator = new EventGenerator();
-			CalendarEvent Event = Generator.CreateNewEvent("Test","A test event", new DateTime(2017, 06, 29, 08, 30, 00), new DateTime(2017, 06, 29, 10, 00, 00), );
+			CalendarEvent Event = Generator.CreateNewEvent("Test","A test event", new DateTime(2017, 06, 29, 08, 30, 00), new DateTime(2017, 06, 29, 10, 00, 00), "Daniel Medic", "Jonat Silveira", new DatabaseContext());
 			CalendarEventList.Add(Event);
 			Model.CalendarEventList = CalendarEventList;
 			return View(Model);
