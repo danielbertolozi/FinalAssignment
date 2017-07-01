229f4111ecca58b24a233a47e81455f94e6d4565
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-07-01 19:29:07 -0300

    * Availability.cs: Small tweaks to Availability Model
    * AvailabilityViewModel.cs: Adds ViewModel for Availability
    
    * Configure.cshtml: Adds basic structure for Configure
    
    * Create.cshtml: Adds View for Availability Create
    
    * Create.html: GBye wrong file

--- FinalAssignment/Models/Availability.cs
+++ FinalAssignment/Models/Availability.cs
@@ -9,17 +9,16 @@ namespace FinalAssignment.Models
 		[Required]
 		public int AvailabilityKey { get; set; }
 		[Required]
-		[Range(1,7)]
+		[Range(0,6)]
 		public int DayOfWeek { get; set; }
 		[Required]
-		[RegularExpression("\\d{2}:\\d{2}", ErrorMessage = "Please provide time in the following fashion: [HH:mm]")]
-		public string StartTime { get; set; }
+		[DataType(DataType.Time)]
+		public TimeSpan StartTime { get; set; }
 		[Required]
-		[RegularExpression("\\d{2}:\\d{2}", ErrorMessage = "Please provide time in the following fashion: [HH:mm]")]
-		public string EndTime { get; set; }
+		[DataType(DataType.Time)]
+		public TimeSpan EndTime { get; set; }
 		[Required]
-		[Range(0,1)]
-		public int IsAllDay { get; set; }
+		public bool IsAllDay { get; set; }
 		public virtual Medics Medics { get; set; }
 	}
 }
---
+++ FinalAssignment/ViewModels/AvailabilityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FinalAssignment.Models;
+
+namespace FinalAssignment.ViewModels
+{
+	public class AvailabilityViewModel
+	{
+		public List<Availability>[] AvailabilityList = new List<Availability>[7];
+	}
+}
