156e3b51263abf3cc4b54939401eb0b83f59b269
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-07-01 22:48:36 -0300

    Adds EventManager functionality for verifying consult type

--- FinalAssignment/Util/EventManager.cs
+++ FinalAssignment/Util/EventManager.cs
@@ -1,48 +1,35 @@
 ﻿using System;
 using FinalAssignment.Models;
 using FinalAssignment.Data;
+using System.Linq;
 
 namespace FinalAssignment.Util
 {
 	public class EventManager
 	{
 		private readonly UserManager _UserManager;
-		private readonly Object User;
+		private readonly Patients _PatientAccount;
+		private readonly DatabaseContext _Context;
 
 		public EventManager(string UserMail, UserManager UserManager)
 		{
 			this._UserManager = UserManager;
 			try
 			{
-				this.User = this._UserManager.RetrieveMedicByEmail(UserMail);
+				this._PatientAccount = this._UserManager.RetrievePatientByEmail(UserMail);
 			}
 			catch (Exception e)
 			{
 				Console.Write(e);
 			}
-			if (this.User == null)
-			{
-				try
-				{
-					this.User = this._UserManager.RetrievePatientByEmail(UserMail);
-				}
-				catch (Exception e)
-				{
-					Console.Write(e);
-				}
-			}
 			return;
 
 		}
-		public EventManager(Medics Medic, UserManager UserManager)
-		{
-			this._UserManager = UserManager;
-			this.User = Medic;
-		}
+
 		public EventManager(Patients Patient, UserManager UserManager)
 		{
 			this._UserManager = UserManager;
-			this.User = Patient;
+			this._PatientAccount = Patient;
 		}
 
 		public void CheckAgainst()
@@ -51,9 +38,28 @@ namespace FinalAssignment.Util
 			return;
 		}
 
-		public void ReClassifyConsult()
+		public int ReClassifyConsult()
 		{
-			/* This method should apply the business rule as defined by the exercise sheet */
+			var PatientKey = this._PatientAccount.PatientKey;
+			if (CheckAgainstFirstConsult(PatientKey))
+			{
+				return 1;
+			}
+			if (CheckAgainstRoutine(PatientKey))
+			{
+				return 2;
+			}
+			return 3;
+		}
+
+		private bool CheckAgainstFirstConsult(int PatientKey)
+		{
+			return _Context.Consults.Where(t => t.PatientKey == PatientKey).ToArray().Length <= 0;
+		}
+
+		private bool CheckAgainstRoutine(int PatientKey)
+		{
+			return DateTime.Now.Subtract(_Context.Consults.Where(t => t.PatientKey == PatientKey).OrderBy(t => t.Date).LastOrDefault().Date) > new TimeSpan(30, 0, 0, 0); 
 		}
 	}
 }
