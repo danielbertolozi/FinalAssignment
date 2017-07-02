9c92312c9ae8f3f182da0829f37332b6f92b47c3
Daniel Bertolozi <danielbertolozi@gmail.com>
2017-07-01 22:32:56 -0300

    Adds constructors and methods placeholders

--- FinalAssignment/Util/EventManager.cs
+++ FinalAssignment/Util/EventManager.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using FinalAssignment.Models;
+using FinalAssignment.Data;
+
 namespace FinalAssignment.Util
 {
 	public class EventManager
 	{
-		public EventManager()
+		private readonly UserManager _UserManager;
+		private readonly Object User;
+
+		public EventManager(string UserMail, UserManager UserManager)
 		{
+			this._UserManager = UserManager;
+			try
+			{
+				this.User = this._UserManager.RetrieveMedicByEmail(UserMail);
+			}
+			catch (Exception e)
+			{
+				Console.Write(e);
+			}
+			if (this.User == null)
+			{
+				try
+				{
+					this.User = this._UserManager.RetrievePatientByEmail(UserMail);
+				}
+				catch (Exception e)
+				{
+					Console.Write(e);
+				}
+			}
+			return;
+
+		}
+		public EventManager(Medics Medic, UserManager UserManager)
+		{
+			this._UserManager = UserManager;
+			this.User = Medic;
+		}
+		public EventManager(Patients Patient, UserManager UserManager)
+		{
+			this._UserManager = UserManager;
+			this.User = Patient;
+		}
+
+		public void CheckAgainst()
+		{
+			/* This method should verify against someone else to check if the agendas match */
+			return;
+		}
+
+		public void ReClassifyConsult()
+		{
+			/* This method should apply the business rule as defined by the exercise sheet */
 		}
 	}
 }
